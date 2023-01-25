5c97b6d7cc80f970fee4509aa8f2f63fb3951e16
alexx73 <tristanluna@yahoo.com.ar>
2023-01-25 12:51:40 -0300

    Updates

---
+++ precache-manifest.2085560ff6bb3844a4896bc8c1e50542.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "44da7fcce90d796a6252",
+    "url": "/github.com.io/ecomm/static/css/main.8b21be64.chunk.css"
+  },
+  {
+    "revision": "44da7fcce90d796a6252",
+    "url": "/github.com.io/ecomm/static/js/main.44da7fcc.chunk.js"
+  },
+  {
+    "revision": "9c38fab293ccfa5c66be",
+    "url": "/github.com.io/ecomm/static/js/1.9c38fab2.chunk.js"
+  },
+  {
+    "revision": "66bdeace376bf79dea5c",
+    "url": "/github.com.io/ecomm/static/js/runtime~main.66bdeace.js"
+  },
+  {
+    "revision": "b81b85022caedd12d9cdb27978dc7fcf",
+    "url": "/github.com.io/ecomm/static/media/guitar-logo.b81b8502.png"
+  },
+  {
+    "revision": "8d2159bd6dc9ceada4a80aed38ac1b39",
+    "url": "/github.com.io/ecomm/index.html"
+  }
+];
