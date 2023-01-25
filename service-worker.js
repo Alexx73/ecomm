d5cb7623fa5a502f6c39402ff677f9a7e50cb6c8
alexx73 <tristanluna@yahoo.com.ar>
2023-01-25 12:54:49 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/github.com.io/ecomm/precache-manifest.2085560ff6bb3844a4896bc8c1e50542.js"
+  "/github.io/ecomm/precache-manifest.3a1b7dc2eb40a43227aee2233503356c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/github.com.io/ecomm/index.html", {
+workbox.routing.registerNavigationRoute("/github.io/ecomm/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
